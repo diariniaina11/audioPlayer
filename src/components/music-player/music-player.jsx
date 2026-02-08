97f926d69f0b473d57af4c8b8e51a6955bd9c766
diariniaina11 <jm.diariniaina@gmail.com>
2026-02-08 09:52:02 +0300

    Creating hook use-audio-player.jsx

--- src/components/music-player/music-player.jsx
+++ src/components/music-player/music-player.jsx
@@ -1,15 +1,21 @@
+import { useRef, useState } from "react"
 import styles from "./music-player.module.css" 
+import { useAudioPlayer } from "../../hooks/use-audio-player"
 
 export function MusicPlayer(){
+    const {isPlaying, currentTime, duration, play, pause} = useAudioPlayer(".");
+
+
     return (
+        <>
         <div className={styles.container}>
-            <button className={styles.buttonPrimary}>Play</button>
+            <button className={styles.buttonPrimary}>{(isPlaying)? "Play":"Pause"}</button>
             <div className={styles.timer}>
                 <span>0:00</span>/ <span>3:00</span>
             </div>
             <progress className={styles.progressbar} value={1} max={3}></progress>
-            
-        </div>
 
+        </div>
+        </>
     )
 }
--- src/hooks/use-audio-player.jsx
+++ src/hooks/use-audio-player.jsx
@@ -1,3 +1,38 @@
-export function useAudioPlayer(audio){
+import { useEffect, useRef, useState } from "react"
+
+export function useAudioPlayer(audioSrc){
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [duration, setDuration] = useState(0)
+    const audioRef = useRef(new Audio(audioSrc));
     
+    useEffect(()=>{
+        const audio =audioRef.current
+        const updateCurrentTime = () => setCurrentTime(audio.currentTime);
+        const updateDuration = ()=> setDuration(audio.duration);
+
+        audio.addEventListener("timeupdate", updateCurrentTime);
+        audio.addEventListener("loadedmetadata", updateDuration);
+        return ()=>{    
+            audio.removeEventListener("timeupdate", updateCurrentTime);
+            audio.removeEventListener("loadedmetadata", updateDuration);     
+        };
+    }, []);
+    const play =()=>{
+        audioRef.current.play();
+        setIsPlaying(true);
+    }     
+    const pause =()=>{
+        audioRef.current.pause();
+        setIsPlaying(false);
+    }     
+    return  {
+        isPlaying,
+        currentTime,
+        duration,
+        play,
+        pause,
+
+    };
+        
 }
