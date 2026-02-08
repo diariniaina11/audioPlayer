edbeeee3c48ce042c3558aebed6ee7ea0536d52b
diariniaina11 <jm.diariniaina@gmail.com>
2026-02-08 10:12:53 +0300

    managing pause and play buttons

--- src/components/music-player/music-player.jsx
+++ src/components/music-player/music-player.jsx
@@ -1,15 +1,18 @@
 import { useRef, useState } from "react"
+import { FaPlay, FaPause } from "react-icons/fa";
+
 import styles from "./music-player.module.css" 
 import { useAudioPlayer } from "../../hooks/use-audio-player"
 
 export function MusicPlayer(){
-    const {isPlaying, currentTime, duration, play, pause} = useAudioPlayer(".");
+    const {isPlaying, currentTime, duration, play, pause} = useAudioPlayer("./audio/WIZKING-GAVIRIA.mp3");
 
 
     return (
         <>
         <div className={styles.container}>
-            <button className={styles.buttonPrimary}>{(isPlaying)? "Play":"Pause"}</button>
+            <button className={styles.buttonPrimary} onClick={(isPlaying ? pause : play)}>{isPlaying ? <FaPause /> : <FaPlay />}
+</button>
             <div className={styles.timer}>
                 <span>0:00</span>/ <span>3:00</span>
             </div>
